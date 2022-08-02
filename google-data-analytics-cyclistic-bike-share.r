5d2a8aa34a51da236f12bcb52b54cec13b26566d
aaa-63 <105097339+aaa-63@users.noreply.github.com>
2022-08-02 18:13:55 -0500

    Kaggle Notebook | Google Data Analytics - Cyclistic Bike-Share  | Version 10

--- google-data-analytics-cyclistic-bike-share.r
+++ google-data-analytics-cyclistic-bike-share.r
@@ -1,73 +1,47 @@
 {
  "cells": [
   {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "acc52e4d",
+   "cell_type": "markdown",
+   "id": "ddcf2863",
    "metadata": {
-    "_cell_guid": "dc62f4c6-fef7-49da-8e6e-f1270745a005",
-    "_uuid": "e3d2ecb6-d1d4-48b7-b239-88531da69dbc",
-    "collapsed": false,
-    "execution": {
-     "iopub.execute_input": "2022-08-02T23:07:28.129498Z",
-     "iopub.status.busy": "2022-08-02T23:07:28.128973Z",
-     "iopub.status.idle": "2022-08-02T23:07:28.142778Z",
-     "shell.execute_reply": "2022-08-02T23:07:28.141339Z"
-    },
-    "jupyter": {
-     "outputs_hidden": false
-    },
     "papermill": {
-     "duration": 0.020703,
-     "end_time": "2022-08-02T23:07:28.145367",
+     "duration": 0.002263,
+     "end_time": "2022-08-02T23:13:46.926290",
      "exception": false,
-     "start_time": "2022-08-02T23:07:28.124664",
+     "start_time": "2022-08-02T23:13:46.924027",
      "status": "completed"
     },
     "tags": []
    },
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "This notebook will transcribe the code that was run in RStudio to clean, manipulate, and analyze the data set.\n"
-     ]
-    }
-   ],
    "source": [
-    "print(\"This notebook will transcribe the code that was run in RStudio to clean, manipulate, and analyze the data set.\")"
+    "**This notebook will transcribe the code that was run in RStudio to clean, manipulate, and analyze the data set.**"
    ]
   }
  ],
  "metadata": {
   "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
   },
   "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.7.12"
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
   },
   "papermill": {
    "default_parameters": {},
-   "duration": 11.406159,
-   "end_time": "2022-08-02T23:07:28.868817",
+   "duration": 4.294084,
+   "end_time": "2022-08-02T23:13:47.049348",
    "environment_variables": {},
    "exception": null,
    "input_path": "__notebook__.ipynb",
    "output_path": "__notebook__.ipynb",
    "parameters": {},
-   "start_time": "2022-08-02T23:07:17.462658",
+   "start_time": "2022-08-02T23:13:42.755264",
    "version": "2.3.4"
   }
  },
