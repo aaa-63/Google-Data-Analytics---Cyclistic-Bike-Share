9807d252f2d62ac70abee616d5d5397096352648
aaa-63 <105097339+aaa-63@users.noreply.github.com>
2022-08-02 18:04:06 -0500

    Kaggle Notebook | Google Data Analytics - Cyclistic Bike-Share  | Version 8

---
+++ google-data-analytics-cyclistic-bike-share.r
@@ -0,0 +1,58 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "fb15ee87",
+   "metadata": {
+    "execution": {
+     "iopub.execute_input": "2022-08-02T23:03:53.613437Z",
+     "iopub.status.busy": "2022-08-02T23:03:53.611433Z",
+     "iopub.status.idle": "2022-08-02T23:03:53.764131Z",
+     "shell.execute_reply": "2022-08-02T23:03:53.762297Z"
+    },
+    "papermill": {
+     "duration": 0.159058,
+     "end_time": "2022-08-02T23:03:53.766640",
+     "exception": false,
+     "start_time": "2022-08-02T23:03:53.607582",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": [
+    "#This notebook will transcribe the code that was run in RStudio to clean, manipulate, and analyze the data set."
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 4.083479,
+   "end_time": "2022-08-02T23:03:53.887671",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2022-08-02T23:03:49.804192",
+   "version": "2.3.4"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
